d283072da304001009d1fc9ba299964c83fd4468
that9jaboy3 <olatunjia889@gmail.com>
2023-08-15 01:25:56 +0100

    style:add carousel to nav bar

--- src/Main.js
+++ src/Main.js
@@ -63,7 +63,7 @@ const Main = () => {
 
             <div className='d-lg-flex align-items-center justify-content-around container-sm'>
 
-                <div class="card border border-0" style={{width:'27rem'}}>
+                <div class="card border border-0 m-auto m-lg-0" style={{width:'27rem'}}>
                 <div class="card-body">
                 <h5 class="card-title">Built With Greatness Inside</h5>
                 <p class="card-text">Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.  Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum.</p>
